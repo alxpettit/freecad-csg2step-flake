d697a7140b1ed3cfedf44af6274902a0220af564
Alexandria P. <alxpettit@gmail.com>
2024-04-17 20:07:31 -0600

    cleanup & switch to nuscript

--- flake.nix
+++ flake.nix
@@ -4,15 +4,19 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
+    nuenv.url = "github:DeterminateSystems/nuenv";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, nuenv, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
-        csg2step = pkgs.writeShellScriptBin "csg2step" ''
-          #!/usr/bin/env sh
-          exec env FREECADPATH=${pkgs.freecad} ${pkgs.freecad}/bin/freecadcmd ${./freecad-csg2step.py} $@
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ nuenv.overlays.nuenv ];  
+        };
+        csg2step = pkgs.nuenv.writeScriptBin "csg2step.nu" ''
+          #!/usr/bin/env nu
+          FREECADPATH=${pkgs.freecad} ${pkgs.freecad}/bin/freecadcmd ${./freecad-csg2step.py} $@
         '';
       in
       {
